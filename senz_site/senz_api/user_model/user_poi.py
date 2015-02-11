ccbfae99ef42b5fe83e70d746d958197023a4995
meowoodie <woodielove@163.com>
2015-02-11 23:45:16 +0800

    code on user poi

--- senz_site/senz_api/user_model/user_poi.py
+++ senz_site/senz_api/user_model/user_poi.py
@@ -1,6 +1,8 @@
-from senz_api.lean_cloud.lean_obj import AVObject
+from lean_cloud.lean_obj import AVObject
+from cloud_services.service_api import ServiceAPI
+import json
 
-class UserPOI(AVObject):
+class UserPOI(AVObject, ServiceAPI):
 
     def __init__(self):
         super(UserPOI, self).__init__()
