e8d157c5e23d914a85574551adb55a8baab004da
meowoodie <woodielove@163.com>
2015-02-14 18:14:50 +0800

    code on user senz model

--- senz_site/senz_api/user_model/user_senz.py
+++ senz_site/senz_api/user_model/user_senz.py
@@ -1,6 +1,28 @@
-from senz_api.lean_cloud.lean_obj import AVObject
+from lean_cloud.lean_obj import AVObject
 
 class UserSenz(AVObject):
 
-    def __init__(self):
+    during = {
+        "A_DAY":   0,
+        "A_WEEK":  1,
+        "A_MONTH": 2,
+        "A_YEAR":  3
+    }
+    output_key = (
+        "Motion",
+        "POI",
+        "Sound",
+        "Light"
+    )
+    DEFAULT_SENZ_DURING = during["A_DAY"]
+
+    def __init__(self, user_id=None, during=DEFAULT_SENZ_DURING, **output_tuple):
         super(UserSenz, self).__init__()
+        self.userId = user_id
+        self.outputListDuring   = []
+        self.outputTupleCurrent = {}
+        # Init Current output tuple
+        for key in self.output_key:
+            if output_tuple.has_key(key):
+                self.outputTupleCurrent[key] = output_tuple[key]
+
