aa1b1487a614dd8e5b9de3ee7c77e23e5957fe51
meowoodie <woodielove@163.com>
2015-03-04 16:25:31 +0800

    debug over, start run on the vps

--- senz_site/senz_api/views.py
+++ senz_site/senz_api/views.py
@@ -1,13 +1,35 @@
 import json
-from django.http.response import HttpResponse
+from django.http.response import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from data_handler import DataHandler
+from django.http import JsonResponse
 
+def errorInfo():
+    import sys
+    info = "%s || %s" % (sys.exc_info()[0], sys.exc_info()[1])  # todo log the exception info
+    print info
+    return info
+
+def errorResponses(error=None):
+    if not error:
+        info = errorInfo()
+    else:
+        info = error
+
+    return JsonResponse({"status": 0}, {"errors": info})
+
+
+def successResponses(results):
+
+    return JsonResponse({"status": 1, "results": results})
+
+@csrf_exempt
 def Senz(request):
 
     if request.method == 'POST':
         body = json.loads(request.body)  #body is deprecated
     else:
-        return HttpResponse("Error!")
+        return errorResponses("Error!")
 
     user_mac     = body["userMac"]
     motion_count = body["motionCount"]
@@ -18,4 +40,4 @@ def Senz(request):
     dh = DataHandler(user_mac, during_time=during_time)
     senz_list = dh.getUserSenz(motion_count, poi_count, sound_count)
 
-    return HttpResponse(senz_list[0])
+    return successResponses(senz_list[0])
